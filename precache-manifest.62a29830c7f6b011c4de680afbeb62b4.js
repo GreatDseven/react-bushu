53c4305ec84c71714ed8f1fcd6844811d4189374
GreatDseven <582858152@qq.com>
2019-08-19 16:31:54 +0800

    sas

---
+++ precache-manifest.62a29830c7f6b011c4de680afbeb62b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f40ce194bdf2dd0c05a3f8fe6f3b726",
+    "url": "/react-bushu/index.html"
+  },
+  {
+    "revision": "252417b382d1057aeecc",
+    "url": "/react-bushu/static/js/2.7f4307a5.chunk.js"
+  },
+  {
+    "revision": "186dd2617b3b5204b9ac",
+    "url": "/react-bushu/static/js/main.b8290b97.chunk.js"
+  },
+  {
+    "revision": "b1ea01805cda07faae84",
+    "url": "/react-bushu/static/js/runtime~main.d25e4b4b.js"
+  }
+]);
